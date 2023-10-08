d235a965583090bf810da1f83fb822aaa2181313
Luis Masuelli <luis@datagran.io>
2023-10-08 16:00:44 -0500

    Adding the date/time formats file.

---
+++ core/formats/main.go
@@ -0,0 +1,10 @@
+package formats
+
+var DateTimeFormats = []string{
+	"%Y-%m-%d %H:%M:%S.%f",
+	"%Y-%m-%dT%H:%M:%S.%f",
+	"%Y-%m-%d %H:%M:%S",
+	"%Y-%m-%dT%H:%M:%S",
+}
+
+var DateFormat = "%Y-%m-%d"
