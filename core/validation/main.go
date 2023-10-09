537b1c26b29bd98ed1596d37f59a1f6b17e9529e
Luis Masuelli <luis@datagran.io>
2023-10-09 17:40:26 -0500

    Adding a regex validator function.

--- core/validation/main.go
+++ core/validation/main.go
@@ -1,10 +1,33 @@
 package validation
 
-import "github.com/go-playground/validator/v10"
+import (
+	"github.com/go-playground/validator/v10"
+	"reflect"
+	"regexp"
+)
+
+var (
+	rxMongoName = regexp.MustCompile("^[a-zA-Z][a-zA-Z0-9_-]+$")
+)
+
+// regexFunction creates a new regex-validator function.
+func regexFunction(regex *regexp.Regexp) func(fl validator.FieldLevel) bool {
+	return func(fl validator.FieldLevel) bool {
+		field := fl.Field()
+		switch field.Kind() {
+		case reflect.String:
+			return regex.MatchString(field.String())
+		default:
+			return false
+		}
+	}
+}
 
 // Validate uses the validator framework, actually.
 // The validation is run against the class, so no
 // further schema is needed.
 func Validate(value any) error {
+	v := validator.New()
+	v.RegisterValidation("mdb-name", regexFunction(rxMongoName))
 	return (validator.New()).Struct(value)
 }
