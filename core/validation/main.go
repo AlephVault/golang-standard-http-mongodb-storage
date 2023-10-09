8b68a90c1e3e821dcc0362be5c2af75c50419ba5
Luis Masuelli <luis@datagran.io>
2023-10-09 16:08:08 -0500

    Removing a .

--- core/validation/main.go
+++ core/validation/main.go
@@ -4,7 +4,7 @@ import "github.com/go-playground/validator/v10"
 
 // Validate uses the validator framework, actually.
 // The validation is run against the class, so no
-// further schema is needed..
+// further schema is needed.
 func Validate(value any) error {
 	return (&validator.Validate{}).Struct(value)
 }
