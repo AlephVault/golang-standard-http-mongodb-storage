b06284c31733f6a33e49861b708fc76bbaa3dba5
Luis Masuelli <luis@datagran.io>
2023-10-09 16:07:40 -0500

    Adding a dumb validation function.

---
+++ core/validation/main.go
@@ -0,0 +1,10 @@
+package validation
+
+import "github.com/go-playground/validator/v10"
+
+// Validate uses the validator framework, actually.
+// The validation is run against the class, so no
+// further schema is needed..
+func Validate(value any) error {
+	return (&validator.Validate{}).Struct(value)
+}
