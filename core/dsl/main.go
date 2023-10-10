cb91904c9f63e53c245e53b7d9453d0061da58c8
Luis Masuelli <luis@datagran.io>
2023-10-10 11:42:37 -0500

    Adding some dive-validators and returning the same object in the Prepare() method.

--- core/dsl/main.go
+++ core/dsl/main.go
@@ -4,13 +4,15 @@ package dsl
 type Settings struct {
 	Debug      bool
 	Connection Connection
-	Global     Global `validate:"dive"`
-	Auth       Auth   `validate:"dive"`
+	Global     Global              `validate:"dive"`
+	Auth       Auth                `validate:"dive"`
+	Resources  map[string]Resource `validate:"dive,keys,mdb-name,endkeys"`
 }
 
 // Prepare prepares the default values of all the members.
-func (settings *Settings) Prepare() {
+func (settings *Settings) Prepare() *Settings {
 	settings.Global.Prepare()
 	settings.Connection.Prepare()
 	settings.Auth.Prepare()
+	return settings
 }
