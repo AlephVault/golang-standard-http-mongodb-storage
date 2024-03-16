084fdbf5add442780c0c274d09f9945e5fae7410
Luis Masuelli <luisfmasuelli@gmail.com>
2024-03-15 19:24:26 -0500

    Adding a common file for the middleware.

---
+++ app/common.go
@@ -0,0 +1,79 @@
+package app
+
+import (
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"standard-http-mongodb-storage/core/auth"
+	"standard-http-mongodb-storage/core/responses"
+	"strings"
+	"time"
+)
+
+// checkPermission checks whether a permission is in the list of permissions.
+func checkPermission(permission string, existingPermissions []interface{}) bool {
+	for _, existingPermission := range existingPermissions {
+		if perm, ok := existingPermission.(string); ok && (perm == "*" || perm == permission) {
+			return true
+		}
+	}
+	return false
+}
+
+// authenticate performs an authentication and permissions check.
+func authenticate(ctx *gin.Context, collection *mongo.Collection, key, permission string) bool {
+	token := ctx.GetHeader("Authorization")
+	if token == "" {
+		responses.AuthMissing(ctx)
+		return false
+	}
+	if !strings.HasPrefix(token, "Bearer ") {
+		responses.AuthBadScheme(ctx)
+		return false
+	}
+
+	token = token[7:]
+	var tokenRecord auth.AuthToken
+	if result := collection.FindOne(ctx, bson.M{
+		"api-key": token, "valid_until": bson.M{
+			"$not": bson.M{"$lt": time.Now()},
+		},
+	}); result.Err() != nil {
+		responses.AuthNotFound(ctx)
+		return false
+	} else if err := result.Decode(&tokenRecord); err != nil {
+		responses.InternalError(ctx)
+		return false
+	}
+
+	effectivePermission := ""
+	if permission == "method" {
+		if ctx.Request.Method == "GET" {
+			effectivePermission = "read"
+		} else {
+			effectivePermission = "write"
+		}
+	} else {
+		effectivePermission = "permission"
+	}
+
+	hasPermission := false
+	if globalPermissions, ok := tokenRecord.Permissions["*"]; ok {
+		if globalPermissionsArray, ok := globalPermissions.([]interface{}); ok {
+			hasPermission = checkPermission(effectivePermission, globalPermissionsArray)
+		}
+	}
+	if !hasPermission {
+		if localPermissions, ok := tokenRecord.Permissions[key]; ok {
+			if localPermissionsArray, ok := localPermissions.([]interface{}); ok {
+				hasPermission = checkPermission(effectivePermission, localPermissionsArray)
+			}
+		}
+	}
+	if !hasPermission {
+		responses.AuthForbidden(ctx)
+		return false
+	}
+
+	return true
+}
