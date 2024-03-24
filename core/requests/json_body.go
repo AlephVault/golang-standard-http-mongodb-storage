70c906a5e23124e09b50d7856599a1dab668927b
Luis Masuelli <luisfmasuelli@gmail.com>
2024-03-24 17:37:36 -0500

    Adds a utility to read a JSON body.

---
+++ core/requests/json_body.go
@@ -0,0 +1,31 @@
+package requests
+
+import (
+	"github.com/go-playground/validator/v10"
+	"github.com/labstack/echo/v4"
+	"standard-http-mongodb-storage/core/responses"
+	"strings"
+)
+
+// ReadJSONBody reads a JSON body and, on error, aborts.
+func ReadJSONBody(context echo.Context, validator_ *validator.Validate, body any) (bool, error) {
+	if body == nil {
+		panic("the body must not be nil")
+	}
+
+	if strings.Contains(strings.ToLower(context.Request().Header.Get("Content-Type")), "application/json") {
+		return false, responses.UnexpectedFormat(context)
+	}
+
+	if err := (&echo.DefaultBinder{}).BindBody(context, &body); err != nil {
+		return false, responses.UnexpectedFormat(context)
+	}
+
+	if validator_ != nil {
+		if err := validator_.Struct(&body); err != nil {
+			return false, responses.InvalidFormat(context, err.(validator.ValidationErrors))
+		}
+	}
+
+	return true, nil
+}
