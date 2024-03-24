289c83db9eacce47f7f1edf8a1165a4069b1ccd7
Luis Masuelli <luisfmasuelli@gmail.com>
2024-03-24 18:30:51 -0500

    Removing some logs.

--- core/requests/json_body.go
+++ core/requests/json_body.go
@@ -1,7 +1,6 @@
 package requests
 
 import (
-	"fmt"
 	"github.com/go-playground/validator/v10"
 	"github.com/labstack/echo/v4"
 	"standard-http-mongodb-storage/core/responses"
@@ -15,12 +14,10 @@ func ReadJSONBody(context echo.Context, validator_ *validator.Validate, body any
 	}
 
 	if !strings.Contains(strings.ToLower(context.Request().Header.Get("Content-Type")), "application/json") {
-		fmt.Println("lalalala 1")
 		return false, responses.UnexpectedFormat(context)
 	}
 
 	if err := (&echo.DefaultBinder{}).BindBody(context, &body); err != nil {
-		fmt.Println("lalalala 2")
 		return false, responses.UnexpectedFormat(context)
 	}
 
